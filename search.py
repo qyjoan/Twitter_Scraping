9baca523f38b2ce983a200f285658f5d6fda207d
joanqiu <qyjoan@gmail.com>
2015-01-25 11:07:50 -0500

    switching from chunking to partitioning by date

--- search.py
+++ search.py
@@ -5,17 +5,15 @@ import datetime
 import urllib
 import signal
 import json
-
-# Don't forget to install tweepy
-# pip install tweepy
+import partitions as par
 
 class TweetSerializer:
    out = None
    first = True
    count = 0
-   def start(self):
+   def start(self,d):
       self.count += 1
-      fname = "/Users/joanqiu/MIDS/W205/week2/out/"+"tweets-"+str(self.count)+".json"
+      fname = "/Users/joanqiu/MIDS/W205/week2/out/"+"tweets-"+d+".json"
       self.out = open(fname,"w")
       self.out.write("[\n")
       self.first = True
@@ -35,7 +33,7 @@ class TweetSerializer:
 
 def interrupt(signum, frame):
    print "Interrupted, closing ..."
-   # magic is here
+   # call ts.end when interrupt to ensure resilience
    ts.end()
    exit(1)
 
@@ -54,25 +52,42 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth_handler=auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
 q = urllib.quote_plus(sys.argv[1])  # URL encoded query
-tweet_limit = int(sys.argv[2])
 
 # Additional query parameters:
 #   since: {date}
 #   until: {date}
 # Just add them to the 'q' variable: q+" since: 2014-01-01 until: 2014-01-02"
 
-tweet_cnt = 0
-ts = TweetSerializer()
-ts.start()
-for tweet in tweepy.Cursor(api.search,q=q).items(100):
-   if tweet_cnt < tweet_limit:
-      ts.write(tweet)
-      tweet_cnt += 1
-   else:
-      ts.end()
-      ts.start()
-      ts.write(tweet)
-      tweet_cnt = 1
-ts.end()
+# allowing a max number of tweets per file #
+
+#tweet_limit = int(sys.argv[2])
+#tweet_cnt = 0
+#ts = TweetSerializer()
+#ts.start()
+#for tweet in tweepy.Cursor(api.search,q=q).items(100):
+#   if tweet_cnt < tweet_limit:
+#      ts.write(tweet)
+#      tweet_cnt += 1
+#   else:
+#      ts.end()
+#      ts.start()
+#      ts.write(tweet)
+#      tweet_cnt = 1
+#ts.end()
    # FYI: JSON is in tweet._json
 
+# partition data on facet
+
+start_dt = sys.argv[2]
+end_dt = sys.argv[3]
+start = datetime.datetime.strptime(start_dt, par.xsdDateFormat)
+end = datetime.datetime.strptime(end_dt, par.xsdDateFormat)
+
+for strd in par.date_partition(start, end):
+   ts = TweetSerializer()
+   start_dt = str(strd).split(' ')[0]
+   end_dt = str(strd + datetime.timedelta(days=1)).split(' ')[0]
+   ts.start(start_dt)
+   for tweet in tweepy.Cursor(api.search, q = q, since = start_dt, until = end_dt).items(10):
+      ts.write(tweet)
+   ts.end()
